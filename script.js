a196eb966cf791a4565f3d52aebb415eac38ebc2
Kapil Sahu <116225142+DevKapil-101@users.noreply.github.com>
2023-08-01 18:28:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,84 +1,81 @@
-//your code here
-document.addEventListener('DOMContentLoaded', () => {
-  const gameContainer = document.getElementById('gameContainer');
-  const scoreBoard = document.getElementById('score');
-  let snake = [{ row: 20, col: 1 }];
-  let direction = 'right';
-  let food = { row: 10, col: 10 };
+document.addEventListener("DOMContentLoaded", () => {
+  const gameContainer = document.getElementById("gameContainer");
+  const scoreElement = document.getElementById("score");
+
   let score = 0;
+  let snakeX = 1;
+  let snakeY = 20;
+  let foodX = 10;
+  let foodY = 10;
+  let direction = "right";
+  let intervalId;
 
-  function draw() {
-    // Clear the game container
-    gameContainer.innerHTML = '';
+  function renderSnake() {
+    const snakePixel = document.createElement("div");
+    snakePixel.className = "snakeBodyPixel";
+    snakePixel.style.left = `${snakeX * 40}px`;
+    snakePixel.style.top = `${snakeY * 40}px`;
+    gameContainer.appendChild(snakePixel);
+  }
 
-    // Draw snake body
-    snake.forEach(segment => {
-      const snakePixel = document.createElement('div');
-      snakePixel.className = 'snakeBodyPixel';
-      snakePixel.style.gridColumn = segment.col;
-      snakePixel.style.gridRow = segment.row;
-      gameContainer.appendChild(snakePixel);
-    });
-
-    // Draw food
-    const foodPixel = document.createElement('div');
-    foodPixel.className = 'food';
-    foodPixel.style.gridColumn = food.col;
-    foodPixel.style.gridRow = food.row;
+  function renderFood() {
+    const foodPixel = document.createElement("div");
+    foodPixel.className = "food";
+    foodPixel.style.left = `${foodX * 40}px`;
+    foodPixel.style.top = `${foodY * 40}px`;
     gameContainer.appendChild(foodPixel);
   }
 
+  function removeSnake() {
+    const snakePixels = document.querySelectorAll(".snakeBodyPixel");
+    snakePixels.forEach((pixel) => pixel.remove());
+  }
+
+  function removeFood() {
+    const foodPixel = document.querySelector(".food");
+    foodPixel.remove();
+  }
+
   function moveSnake() {
-    const head = { ...snake[0] };
+    removeSnake();
     switch (direction) {
-      case 'up':
-        head.row--;
+      case "right":
+        snakeX++;
         break;
-      case 'down':
-        head.row++;
+      case "left":
+        snakeX--;
         break;
-      case 'left':
-        head.col--;
+      case "up":
+        snakeY--;
         break;
-      case 'right':
-        head.col++;
+      case "down":
+        snakeY++;
         break;
     }
+    checkCollision();
+    renderSnake();
+  }
 
-    // Check if the snake eats the food
-    if (head.row === food.row && head.col === food.col) {
+  function checkCollision() {
+    if (snakeX === foodX && snakeY === foodY) {
       score++;
-      scoreBoard.textContent = score;
-      generateFood();
-    } else {
-      snake.pop(); // Remove the last segment if not eating food
+      scoreElement.textContent = score;
+      removeFood();
+      spawnFood();
     }
-
-    snake.unshift(head); // Add new head
-    draw();
   }
 
-  function generateFood() {
-    food.row = Math.floor(Math.random() * 20) + 1;
-    food.col = Math.floor(Math.random() * 20) + 1;
-  }
-
-  function handleKeyPress(event) {
-    const key = event.key;
-    if (key === 'ArrowUp' && direction !== 'down') direction = 'up';
-    else if (key === 'ArrowDown' && direction !== 'up') direction = 'down';
-    else if (key === 'ArrowLeft' && direction !== 'right') direction = 'left';
-    else if (key === 'ArrowRight' && direction !== 'left') direction = 'right';
+  function spawnFood() {
+    foodX = Math.floor(Math.random() * 10);
+    foodY = Math.floor(Math.random() * 10);
+    renderFood();
   }
 
   function startGame() {
-    setInterval(moveSnake, 100);
+    spawnFood();
+    renderSnake();
+    intervalId = setInterval(moveSnake, 100);
   }
 
-  // Event listeners
-  document.addEventListener('keydown', handleKeyPress);
-
-  // Start the game
-  generateFood();
   startGame();
 });
